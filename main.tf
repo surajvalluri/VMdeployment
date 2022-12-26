6fdbaabcac1d364814845cd89460f9136cd0821b
surajvalluri <109567679+surajvalluri@users.noreply.github.com>
2022-12-26 12:52:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_subnet" "stagingsubnet" {
   name                 = "stagingSubnet"
   resource_group_name  = azurerm_resource_group.test-terraform.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes      = ["10.0.2.0/24"]
 }
 
 resource "azurerm_subnet" "prodsubnet" {
